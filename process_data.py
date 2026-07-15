26abbc9bf930adc6cfd85c7bf487e31651c5b26e
harurun999 <kanaharu0503@gmail.com>
2026-07-15 14:50:39 +0900

    create init and process input class

--- process_data.py
+++ process_data.py
@@ -4,10 +4,10 @@ class process_input():
         self._start: tuple[str, int, int]
         self._goal: tuple[str, int, int]
         self._waypoints_names: list[str]
-        self._waypoints: dict[str, tuple]
+        self._waypoints: dict[str, tuple[int, int]]
         self._points_metadata: dict[tuple[int, int], list[str]]
-        self._connections: tuple[str, str]
-        self._connections_meadata: dict[tuple, list[str]]
+        self._connections: str
+        self._connections_meadata: dict[str, list[str] | None]
 
     def read_file(self) -> None:
         file_name: str = input("File name?: ")
@@ -33,19 +33,36 @@ class process_input():
                 self._nb_drones = int(tmp_dict[key].strip)
             elif key == "connection":
                 value_list: list[str] = tmp_dict[key].strip.split(" ")
-                if len(value_list) != 2:
-                    raise ValueError("connection must have 2 values.")
-                
+                if len(value_list) == 1:
+                    self._connections = value_list[0]
+                    self._connections_meadata[value_list[0]] = None
+                elif len(value_list) == 2:
+                    self._connections = value_list[0]
+                    self._connections_meadata[value_list[0]] = value_list[1]
+                else:
+                    raise ValueError("connection must have 1 or 2 values.")
             elif key == "start_hub":
                 value_list: list[str] = tmp_dict[key].strip.split(" ")
                 if len(value_list) != 4:
                     raise ValueError("start_hub must have 4 values")
-
+                self._start = tuple(
+                    value_list[0], int(value_list[1]), int(value_list[2])
+                    )
             elif key == "end_hub":
                 value_list: list[str] = tmp_dict[key].strip.split(" ")
                 if len(value_list) != 4:
                     raise ValueError("end_hub must have 4 values")
+                self._goal = tuple(
+                    value_list[0], int(value_list[1]), int(value_list)
+                )
             elif key == "hub":
                 value_list: list[str] = tmp_dict[key].strip.split(" ")
                 if len(value_list) != 4:
                     raise ValueError("end_hub must have 4 values")
+                self._waypoints_names.append(value_list[0])
+                coordinate: tuple[int, int] = tuple(
+                    int(value_list[1]), int(value_list[2])
+                    )
+                self._waypoints[value_list[0]] = coordinate
+                self._points_metadata[coordinate] = value_list[3]
+
